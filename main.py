0eba4aa0392f4c2c2bb66be524805521cb564c5c
Jeremy Phillips <codingadventurestoday@gmail.com>
2025-06-14 20:18:00 -0400

    Created and downloaded the projects requirements within local venv using requirements.txt file. created an initial api call for gemini for the project's foundation

---
+++ main.py
@@ -0,0 +1,20 @@
+import os 
+from dotenv import load_dotenv
+
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+temp_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum"
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=temp_prompt
+)
+
+print(response.text)
+
+print(f"""Prompt tokens: {response.usage_metadata['prompt_token_count']}
+Response tokens: {response.usage_metadata['candidates_token_count']}""")
