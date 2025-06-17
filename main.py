eaa18870b67f4bb593beb7bb27d37d9aa2397a2a
Jeremy Phillips <codingadventurestoday@gmail.com>
2025-06-16 21:34:04 -0400

    moved the api call logic into the generate_content function. Additionally, created the messages list to store user's prompts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
@@ -10,16 +11,23 @@ def main():
     if not isinstance(sys.argv[0], str):
         sys.exit("not a string")
     
+    user_prompt = " ".join(sys.argv[1:])
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents=sys.argv[0]
-    )
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    client = genai.Client(api_key=api_key)
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
     print("Response:")
     print(response.text)
 
